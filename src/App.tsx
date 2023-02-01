0a7e31e6c3373670499b763ad30600137d6395ca
glaubermatos <glaub.oliveira@hotmail.com>
2023-02-01 19:48:54 -0300

    Estrutura da aplicação - Estilos globais com a funcao createGlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from 'styled-components'
 import { Button } from "./components/Button";
+import { GlobalStyle } from './styles/global';
 
 import { defaultTheme } from './styles/themes/default';
 
@@ -11,6 +12,8 @@ export function App() {
       <Button variant="danger" />
       <Button variant="success" />
       <Button />
+      
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
