82ea1659312ae2f2d1a4be2f842df94abd3d8168
glaubermatos <glaub.oliveira@hotmail.com>
2023-02-01 20:18:18 -0300

    Estrutura da aplicação - Configurando ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from "./components/Button";
-import { GlobalStyle } from './styles/global';
+import { Button } from './components/Button'
+import { GlobalStyle } from './styles/global'
 
-import { defaultTheme } from './styles/themes/default';
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
@@ -12,7 +12,7 @@ export function App() {
       <Button variant="danger" />
       <Button variant="success" />
       <Button />
-      
+
       <GlobalStyle />
     </ThemeProvider>
   )
